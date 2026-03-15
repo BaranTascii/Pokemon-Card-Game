c3dc56299b0a79782c1d45cd4bd9e5610282320e
Baran Taşcı <barantasci44@gmail.com>
2026-03-15 13:28:15 +0300

    server.js added

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const cors = require("cors")
+
+const packRoutes = require("./routes/pack")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use(express.json())
+
+app.use("/pack", packRoutes)
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000")
+})

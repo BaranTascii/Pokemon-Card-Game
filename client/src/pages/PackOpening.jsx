58081ca060c46471bf5d8a1d7488ad8791f2aa23
Baran Taşcı <barantasci44@gmail.com>
2026-03-15 13:40:35 +0300

    PackOpening.jsx added

---
+++ client/src/pages/PackOpening.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { openPack } from "../services/api";
+import Card from "../components/Card";
+
+export default function PackOpening() {
+  const [cards, setCards] = useState([]);
+
+  async function handleOpen() {
+    const pack = await openPack();
+
+    setCards(pack);
+  }
+
+  return (
+    <div>
+      <h1>Pokemon Pack Opening</h1>
+      <button onClick={handleOpen}>Open Pack</button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(5,1fr)",
+          gap: "20px",
+          marginTop: "20px",
+        }}
+      >
+        {cards.map((card, index) => (
+          <Card key={index} card={card} />
+        ))}
+      </div>
+    </div>
+  );
+}

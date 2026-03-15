2200610771d2781a4e89d90080c870b720067d99
Baran Taşcı <barantasci44@gmail.com>
2026-03-15 13:29:15 +0300

    packService.js added

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 
-const packRoutes = require("./routes/pack")
+const packRoutes = require("./routes/pack");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/pack", packRoutes)
+app.use("/pack", packRoutes);
 
 app.listen(5000, () => {
-    console.log("Server running on port 5000")
-})
+  console.log("Server running on port 5000");
+});
---
+++ server/services/packService.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+
+async function getRandomPokemon() {
+  const id = Math.floor(Math.random() * 151) + 1;
+
+  const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+  return {
+    id: res.data.id,
+    name: res.data.name,
+
+    image: res.data.sprites.other["official-artwork"].front_default,
+  };
+}
+
+async function openPack() {
+  const cards = [];
+
+  for (let i = 0; i < 10; i++) {
+    const pokemon = await getRandomPokemon();
+
+    cards.push(pokemon);
+  }
+
+  return cards;
+}
+
+module.exports = {
+  openPack,
+};

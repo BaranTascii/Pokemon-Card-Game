c1e84eab61fdfea324ab4f653ba13ea8c9e852db
Baran Taşcı <barantasci44@gmail.com>
2026-03-17 17:43:04 +0300

    api.js updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export async function openPack() {
-  const res = await axios.get("http://localhost:5000/pack/open");
+export async function openPack(userId) {
+  const res = await axios.post("http://localhost:5000/pack/open", { userId });
 
   return res.data;
 }

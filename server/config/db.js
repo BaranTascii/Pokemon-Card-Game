aa8e61285e63a8c8a85004ddfb79b37160a6fcc3
Baran Taşcı <barantasci44@gmail.com>
2026-03-17 18:15:42 +0300

    bugs fixed

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 async function connectDB() {
+  console.log("MONGO_URI:", process.env.MONGO_URI);
   await mongoose.connect(process.env.MONGO_URI);
-
-  console.log("MongoDB Atlas connected");
+  console.log("MongoDB connected");
 }
 
 module.exports = connectDB;

ec9368f005c0c62739faab0bd90aade86631cad0
Baran Taşcı <barantasci44@gmail.com>
2026-03-17 17:38:37 +0300

    packService.js updated

--- server/services/packService.js
+++ server/services/packService.js
@@ -1,17 +1,20 @@
 const axios = require("axios");
+const User = require("../models/User");
 
-const { getRarity } = require("./rarityService.js");
-const { isShiny } = require("./shinyService.js");
+const { getRarity } = require("./rarityService");
+const { isShiny } = require("./shinyService");
+const { getCoinReward } = require("./rewardService");
 
 async function getRandomPokemon() {
   const id = Math.floor(Math.random() * 151) + 1;
+
   const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
 
   const rarity = getRarity();
   const shiny = isShiny();
 
   return {
-    id: res.data.id,
+    pokemonId: res.data.id,
     name: res.data.name,
     image: res.data.sprites.other["official-artwork"].front_default,
     rarity,
@@ -19,12 +22,45 @@ async function getRandomPokemon() {
   };
 }
 
-async function openPack() {
-  const promises = [];
-  for (let i = 0; i < 10; i++) {
-    promises.push(getRandomPokemon());
+async function openPack(userId) {
+  const user = await User.findById(userId);
+
+  if (!user) throw new Error("User not found");
+
+  if (user.coins < 50) {
+    throw new Error("Not enough coins");
   }
-  return await Promise.all(promises);
+
+  user.coins -= 50;
+
+  const cards = await Promise.all(
+    Array.from({ length: 10 }, () => getRandomPokemon()),
+  );
+
+  let earnedCoins = 0;
+
+  cards.forEach((card) => {
+    const exists = user.collection.find(
+      (c) => c.pokemonId === card.pokemonId && c.shiny === card.shiny,
+    );
+
+    if (exists) {
+      const reward = getCoinReward(card.rarity);
+      earnedCoins += reward;
+    } else {
+      user.collection.push(card);
+    }
+  });
+
+  user.coins += earnedCoins;
+
+  await user.save();
+
+  return {
+    cards,
+    coins: user.coins,
+    earnedCoins,
+  };
 }
 
 module.exports = { openPack };

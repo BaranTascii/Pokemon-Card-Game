6e3686ad33a9aeda8e4fcca8dda13368ac264f0a
Baran Taşcı <barantasci44@gmail.com>
2026-03-17 17:21:40 +0300

    packService.js updated

--- server/services/packService.js
+++ server/services/packService.js
@@ -1,27 +1,30 @@
 const axios = require("axios");
 
+const { getRarity } = require("./rarityService.js");
+const { isShiny } = require("./shinyService.js");
+
 async function getRandomPokemon() {
   const id = Math.floor(Math.random() * 151) + 1;
-
   const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+  const rarity = getRarity();
+  const shiny = isShiny();
 
   return {
     id: res.data.id,
     name: res.data.name,
     image: res.data.sprites.other["official-artwork"].front_default,
+    rarity,
+    shiny,
   };
 }
 
 async function openPack() {
-  const cards = [];
-
+  const promises = [];
   for (let i = 0; i < 10; i++) {
-    const pokemon = await getRandomPokemon();
-    cards.push(pokemon);
+    promises.push(getRandomPokemon());
   }
-  return cards;
+  return await Promise.all(promises);
 }
 
-module.exports = {
-  openPack,
-};
+module.exports = { openPack };
